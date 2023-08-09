bd67c36e5cd0df41d885768f290f1bbd9d10feb8
Danny <cozinheiro@me.com>
2023-08-09 16:42:03 +0200

    Send an email with news titles and descriptions

---
+++ send_email.py
@@ -0,0 +1,13 @@
+import smtplib, ssl
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "dannyveugelen@gmail.com"
+    password = "qccyazhxksvspboo"
+    receiver = "cozinheiro@me.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
